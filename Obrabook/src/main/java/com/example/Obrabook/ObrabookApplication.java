2ad37b46a843d3087c96cb07ed3f235bf023b556
Iagopatrick <iagogripp@hotmail.com>
2023-01-18 17:48:25 -0300

    first

---
+++ Obrabook/src/main/java/com/example/Obrabook/ObrabookApplication.java
@@ -0,0 +1,13 @@
+package com.example.Obrabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObrabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObrabookApplication.class, args);
+	}
+
+}
---
+++ Obrabook/src/test/java/com/example/Obrabook/ObrabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Obrabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObrabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
